16d6032d9b609bb9f18ce71c513b3dfa07b0e9ff
pdn002 <pdn002@ucsd.edu>
2022-10-26 16:34:13 -0700

    image and play button implemented

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,30 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    // TODO
+    
+    // set correct image
+    const horn_select = document.getElementById('horn-select');
+    horn_select.addEventListener('change', function(){
+        if (horn_select.value == 'air-horn'){
+            // if (document.querySelector('img')){ console.log('worked');}
+            document.querySelector('img').src = 'assets/images/air-horn.svg';
+            document.querySelector('audio').src = 'assets/audio/air-horn.mp3';
+        }
+        if (horn_select.value == 'car-horn'){
+            document.querySelector('img').src = 'assets/images/car-horn.svg';
+            document.querySelector('audio').src = 'assets/audio/car-horn.mp3';
+        }
+        if (horn_select.value == 'party-horn'){
+            document.querySelector('img').src = 'assets/images/party-horn.svg';
+            document.querySelector('audio').src = 'assets/audio/party-horn.mp3';
+        }
+    })
+
+    // implement sound for play button
+    const play_button = document.querySelector('button');
+    play_button.addEventListener('click', function(){
+        document.querySelector('audio').play();
+    })
+
+    // implement volume controls
 }
