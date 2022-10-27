7efe6d4388eccba606b7c0d181c71e816e72a12b
pdn002 <pdn002@ucsd.edu>
2022-10-26 23:55:43 -0700

    speech is done just need to move smiley face

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,32 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    // TODO
+    const synth = window.speechSynthesis;
+    const voice_select = document.getElementById('voice-select');
+    const text = document.getElementById('text-to-speak');
+    const button = document.querySelector('button');
+
+    // need timer to give time for voice list to populate after loading
+    setTimeout(function(){
+        let voices = synth.getVoices();
+
+        // adds each option like in HTML
+        // <option value="value" disabled selected>innerHTML</option>
+        for (let i = 0; i < voices.length; i++){
+            let option = document.createElement('option');
+            option.value = i;
+            option.innerHTML = voices[i].name;
+            voice_select.appendChild(option);
+        }
+
+        // implement speech button
+        button.addEventListener('click', function(){
+            let say_this = new SpeechSynthesisUtterance(text.value);
+            say_this.voice = voices[voice_select.value];
+            synth.speak(say_this);
+        })
+
+        
+    }, 50);
+    
 }
